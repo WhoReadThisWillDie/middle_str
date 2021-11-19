8ca14b9230743a2bd5ebb884d881d4909ebbb156
WhoReadThisWillDie <78028560+WhoReadThisWillDie@users.noreply.github.com>
2021-11-20 00:03:32 +0300

    Update middle_str.h

--- middle_str.h
+++ middle_str.h
@@ -18,7 +18,6 @@ string itc_maxCharWord(string str);
 char itc_sameChar(string str);
 bool itc_isFirstInSecond(string s1, string s2);
 string itc_Cezar(string str, int k);
-string itc_Cezar1(string str, int k);
 string itc_rmFreeSpace(string str);
 bool itc_isIp(string str);
 string itc_DecToBin(string str);
